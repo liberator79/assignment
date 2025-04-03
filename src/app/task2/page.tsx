1548dca6e9ef57e712b4ab0c623b1a3038600963
liberator79 <s191037@rguktsklm.ac.in>
2025-04-03 17:01:17 +0530

    changed task2

--- src/app/task2/page.tsx
+++ src/app/task2/page.tsx
@@ -24,29 +24,27 @@ const Task2Page = () => {
   const resetAllSquares = () => {
     setResetInProgress(true);
     let delay = 0;
-    squaresInd.forEach((index, _unused) => {
-      setTimeout(() => {
-        setSquares((prev) => {
-          const newSquares = [...prev];
-          newSquares[index] = !newSquares[index];
-          return newSquares;
-        });
-      }, delay);
-      delay += 300;
+    const resetPromises = squaresInd.map((index) => {
+      return new Promise<void>((resolve) => {
+        setTimeout(() => {
+          setSquares((prev) => {
+            const newSquares = [...prev];
+            newSquares[index] = false;
+            return newSquares;
+          });
+          resolve();
+        }, delay);
+        delay += 300;
+      });
     });
-    setSquaresInd([]);
-    setResetInProgress(false);
+    Promise.all(resetPromises).then(() => {
+      setSquaresInd([]);
+      setResetInProgress(false);
+    });
   };
   return (
     <div className="h-[100vh] w-full flex flex-col items-center justify-center bg-black">
-      <div className="p-1">
-        {resetInProgress && (
-          <div className="flex gap-2">
-            <div className="font-bold text-xl">Resetting</div>
-            <div className="w-7 h-7 border-3 border-black border-t-white rounded-full animate-spin"></div>
-          </div>
-        )}
-      </div>
+      <div className="text-2xl font-bold p-4">Task 2</div>
       <div className="grid grid-cols-3 gap-2">
         {squares.map((isGreen, index) => (
           <div
@@ -57,6 +55,14 @@ const Task2Page = () => {
             }`}
           />
         ))}
+      </div>
+      <div className="p-4">
+        {resetInProgress && (
+          <div className="flex gap-2">
+            <div className="font-bold text-xl">Resetting</div>
+            <div className="w-7 h-7 border-3 border-black border-t-white rounded-full animate-spin"></div>
+          </div>
+        )}
       </div>
     </div>
   );
